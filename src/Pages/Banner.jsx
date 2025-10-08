56349c0388ff2fbd00ced034abdb61d1c65e662f
Abdur Rahman <abdur@example.com>
2025-10-08 11:31:50 +0600

    created States section and updated Home section and Banner section

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router';
 
 const Banner = () => {
     return (
-      <div className="flex flex-col items-center text-center px-4 sm:px-6 md:px-8 lg:px-16 py-12 md:py-16 lg:py-24 space-y-6">
+      <div className="flex flex-col items-center text-center px-4 sm:px-6 md:px-8 lg:px-16 pt-12 md:pt-16 lg:pt-24 space-y-6">
     {/* Heading */}
   <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold leading-tight">
     We Build <br />
@@ -14,7 +14,7 @@ const Banner = () => {
   </h1>
 
    {/* Description */}
- <p className="text-gray-600 max-w-xs sm:max-w-md md:max-w-xl lg:max-w-2xl text-base sm:text-lg md:text-xl">
+ <p className="text-gray-600 max-w-xs sm:max-w-md md:max-w-xl lg:max-w-3xl text-base sm:text-lg md:text-xl">
   HERO.IO builds cutting-edge apps that simplify your daily life, boost productivity, and inspire creativity.  
   We transform ideas into seamless digital experiences that make a real impact for users worldwide.
 </p>
@@ -24,7 +24,7 @@ const Banner = () => {
   <div className="flex flex-col sm:flex-row gap-4 mt-4 justify-center w-full max-w-md sm:max-w-lg md:max-w-xl">
     {/* Google Play */}
     <a
-      href="#"
+      href="https://play.google.com/store/apps?hl=en"
       className="flex items-center justify-center gap-3 bg-gray-100 hover:bg-gray-200 px-5 py-3 rounded-lg shadow-md transition-all duration-300 w-full sm:w-auto"
     >
       <img src="/src/assets/playStore.png" alt="Google Play" className="w-6 h-6 sm:w-8 sm:h-8" />
@@ -33,13 +33,22 @@ const Banner = () => {
 
     {/* App Store */}
     <a
-      href="#"
+      href="https://www.apple.com/app-store/"
       className="flex items-center justify-center gap-3 bg-gray-100 hover:bg-gray-200 px-5 py-3 rounded-lg shadow-md transition-all duration-300 w-full sm:w-auto"
     >
       <img src="/src/assets/appStore.png" alt="App Store" className="w-6 h-6 sm:w-8 sm:h-8" />
       <span className="font-semibold text-gray-800 text-sm sm:text-base">App Store</span>
     </a>
   </div>
+
+   {/* Hero image */}
+   <div className=' max-w-[320px] sm:max-w-[500px] md:max-w-[650px] lg:max-w-[800px] xl:max-w-[900px] mt-6 mx-auto'>
+    <img
+     src="/src/assets/hero.png"
+     alt="Hero"
+     className='w-full max-w-full h-auto object-contain'
+     />
+   </div>
       </div>
 
     );
